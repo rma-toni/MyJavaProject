472c18298a5a26d3e0157d3388f8638ed88ee456
rma-toni <rmaflores.sw@gmail.com>
2024-08-30 18:22:20 -0300

    Snippet added

---
+++ CodeSnippets/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        //Objets
+        String hi = "Hello World!";
+
+        //Primitive
+        int numero = 10;
+        boolean value = true;
+        
+        //Dynamic
+        var dato1 = 14;
+        var dato2 = "Hello!";
+    }
+}
--- MainProject/src/App.java
+++ MainProject/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String saludar = "Bienvenido!";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+
+
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        boolean valor = true;
     }
 }
