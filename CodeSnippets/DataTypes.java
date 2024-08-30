99e172da7e193cdff58a48cc81bbd10762b82ae4
rma-toni <rmaflores.sw@gmail.com>
2024-08-30 19:00:49 -0300

    Snippet modified

--- CodeSnippets/DataTypes.java
+++ CodeSnippets/DataTypes.java
@@ -5,8 +5,14 @@ public class DataTypes {
         String hi = "Hello World!";
 
         //Primitive
-        int numero = 10;
-        boolean value = true;
+        byte verySmallNumber = 10; //8 bits
+        short smallNumber = 23000; //16 bits
+        int numero = 4020000; //32 bits
+        long longNumber = 352300000; //64 bits
+        float realFloat = 3.1416f; //32 bits
+        double realDouble = 4.7095324E3; //64 bits
+        boolean value = true; //1 bits
+        char caracter = 'a'; //16 bits
         
         //Dynamic
         var dato1 = 14;
